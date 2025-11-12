f498ac7c23192dc0008002cb9de23ca89a731459
Vinicius Rech <vinicius.rech.dev@gmail.com>
2025-11-12 00:51:08 -0300

    feat: implement camera centering and improve grid color.

--- src/core/map/map.rs
+++ src/core/map/map.rs
@@ -1,7 +1,9 @@
 use crate::core::map::tile::{Dimensions, Tile};
-use macroquad::color::YELLOW;
-use macroquad::shapes::{draw_rectangle, draw_rectangle_lines};
-use macroquad::window::screen_width;
+use macroquad::camera::{Camera2D, set_camera, set_default_camera};
+use macroquad::color::GRAY;
+use macroquad::math::vec2;
+use macroquad::shapes::draw_rectangle_lines;
+use macroquad::window::{screen_height, screen_width};
 
 pub struct Map {
     map_dimension: Dimensions,
@@ -21,16 +23,22 @@ impl Map {
     }
 
     fn generate_grid(&self) {
-        let half_tile_width = self.tile_dimensions.width / 2;
-        let half_tile_height = self.tile_dimensions.height / 2;
+        let grid_width = self.map_dimension.width as f32 * self.tile_dimensions.width as f32;
+        let grid_height = self.map_dimension.height as f32 * self.tile_dimensions.height as f32;
 
-        let screen_center_x: f32 = (screen_width() / 2.0) - half_tile_width as f32;
-        let screen_center_y: f32 = (screen_width() / 2.0) - half_tile_height as f32;
+        let grid_center_x = grid_width / 2.0;
+        let grid_center_y = grid_height / 2.0;
 
-        for row in 0..self.map_dimension.height as i32 {
-            for col in 0..self.map_dimension.width as i32 {
-                let x: f32 = screen_center_x + col as f32 * self.tile_dimensions.width as f32;
-                let y: f32 = screen_center_y + row as f32 * self.tile_dimensions.height as f32;
+        set_camera(&Camera2D {
+            target: vec2(grid_center_x, grid_center_y),
+            zoom: vec2(1.0 / screen_width() * 2.0, 1.0 / screen_height() * 2.0),
+            ..Default::default()
+        });
+
+        for row in 0..self.map_dimension.height {
+            for col in 0..self.map_dimension.width {
+                let x = col as f32 * self.tile_dimensions.width as f32;
+                let y = row as f32 * self.tile_dimensions.height as f32;
 
                 draw_rectangle_lines(
                     x,
@@ -38,9 +46,11 @@ impl Map {
                     self.tile_dimensions.width as f32,
                     self.tile_dimensions.height as f32,
                     1.0,
-                    YELLOW,
+                    GRAY,
                 );
             }
         }
+
+        set_default_camera();
     }
 }
