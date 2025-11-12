afb0ef53f1e7580ebeabec757724c98f08546359
Vinicius Rech <vinicius.rech.dev@gmail.com>
2025-11-12 00:43:20 -0300

    fix: change rectangles to bordered rectangles grid.

--- src/core/map/map.rs
+++ src/core/map/map.rs
@@ -1,6 +1,6 @@
 use crate::core::map::tile::{Dimensions, Tile};
 use macroquad::color::YELLOW;
-use macroquad::shapes::draw_rectangle;
+use macroquad::shapes::{draw_rectangle, draw_rectangle_lines};
 use macroquad::window::screen_width;
 
 pub struct Map {
@@ -32,11 +32,12 @@ impl Map {
                 let x: f32 = screen_center_x + col as f32 * self.tile_dimensions.width as f32;
                 let y: f32 = screen_center_y + row as f32 * self.tile_dimensions.height as f32;
 
-                draw_rectangle(
+                draw_rectangle_lines(
                     x,
                     y,
                     self.tile_dimensions.width as f32,
                     self.tile_dimensions.height as f32,
+                    1.0,
                     YELLOW,
                 );
             }
